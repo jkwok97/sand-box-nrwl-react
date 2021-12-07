b8b584d0599b5d5d8c24ca71bb777acb7c506870
JeffKwok <jeff.k@decisivefarming.com>
2021-12-07 14:05:23 -0700

    Portals

--- apps/user-list/src/components/UI/ErrorModal/ErrorModal.tsx
+++ apps/user-list/src/components/UI/ErrorModal/ErrorModal.tsx
@@ -1,26 +1,57 @@
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import Button from '../Button/Button';
 import Card from '../Card/Card';
 import styles from './ErrorModal.module.css';
 import { ErrorModalProps } from './ErrorModal.props';
 
+const Backdrop = (props: ErrorModalProps) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props: ErrorModalProps) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+
+      <footer className={styles.actions}>
+        <Button type="button" onClick={props.onConfirm}>
+          OK
+        </Button>
+      </footer>
+    </Card>
+  );
+};
+
+const backDropRoot: Element = document.getElementById(
+  'backdrop-root'
+) as HTMLElement;
+const overlayRoot: Element = document.getElementById(
+  'overlay-root'
+) as HTMLElement;
+
 const ErrorModal = (props: ErrorModalProps) => {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onConfirm}/>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-
-        <footer className={styles.actions}>
-          <Button type="button" onClick={props.onConfirm}>OK</Button>
-        </footer>
-      </Card>
-    </div>
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        backDropRoot
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        overlayRoot
+      )}
+    </Fragment>
   );
 };
 
