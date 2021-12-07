61d48f4f963fe8fd2d8b5152af8b535d418dd3b3
JeffKwok <jeff.k@decisivefarming.com>
2021-12-07 15:59:42 -0700

    useEffect

--- apps/login/src/app/app.tsx
+++ apps/login/src/app/app.tsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import styles from './app.module.css';
 import MainHeader from '../components/MainHeader/MainHeader';
 import Login from '../components/Login/Login';
@@ -7,11 +7,23 @@ import Home from '../components/Home/Home';
 export function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const isUserLoggedIn = localStorage.getItem('isLoggedIn');
+
+    if (isUserLoggedIn === '1') {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   const loginHandler = (email: string, password: string) => {
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
