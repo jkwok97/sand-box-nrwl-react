62e1fdacbba206c67cf63eb2f2808c208083fab5
JeffKwok <jeff.k@decisivefarming.com>
2021-12-15 08:11:42 -0700

    forms start

---
+++ apps/forms/src/app/app.tsx
@@ -0,0 +1,12 @@
+import SimpleInput from '../components/SimpleInput';
+import styles from './app.module.css';
+
+export function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
+}
+
+export default App;
